f3b8f5f2b5a62521f2b0e01a425e30fc22f31a4b
PadBro <padmaster2012@googlemail.com>
2025-06-07 18:44:24 +0200

    chore: improve cache setting

--- src/client/java/com/padbro/greeterbro/client/config/CacheClearType.java
+++ src/client/java/com/padbro/greeterbro/client/config/CacheClearType.java
@@ -1,8 +1,16 @@
 package com.padbro.greeterbro.client.config;
 
-public enum CacheClearType {
+import me.shedaniel.clothconfig2.gui.entries.SelectionListEntry;
+import org.jetbrains.annotations.NotNull;
+
+public enum CacheClearType implements SelectionListEntry.Translatable  {
   OnJoin,
   OnNewSession,
   OnNewDay,
-  Never,
+  Never;
+
+  @Override
+  public @NotNull String getKey() {
+    return "text.autoconfig.GreeterBro.option.returningPlayerConfig.cacheClearType." + toString().toLowerCase();
+  }
 }
--- src/client/java/com/padbro/greeterbro/client/config/ReturningPlayerConfig.java
+++ src/client/java/com/padbro/greeterbro/client/config/ReturningPlayerConfig.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.EnumHandler.EnumDisplayOption;
 
 @Config(name = "returningPlayer")
 public class ReturningPlayerConfig implements ConfigData {
@@ -13,7 +14,9 @@ public class ReturningPlayerConfig implements ConfigData {
   @ConfigEntry.BoundedDiscrete(max = 10)
   public int ignoreForMin = 5;
 
-  @ConfigEntry.Gui.Tooltip public CacheClearType cacheClearType = CacheClearType.OnJoin;
+  @ConfigEntry.Gui.Tooltip
+  @ConfigEntry.Gui.EnumHandler(option = EnumDisplayOption.BUTTON)
+  public CacheClearType cacheClearType = CacheClearType.OnJoin;
 
   @ConfigEntry.Gui.Tooltip public List<String> greetings = List.of("Welcome back", "wb", "o/");
 
