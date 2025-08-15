0b8e2a308e311b8a0023b127cd43b559c16aaf22
PadBro <padmaster2012@googlemail.com>
2025-08-15 11:52:28 +0200

    feat: add to cache on join

---
+++ src/client/java/com/padbro/greeterbro/client/mixin/OnPlayerListMixin.java
@@ -0,0 +1,30 @@
+package com.padbro.greeterbro.client.mixin;
+
+import com.padbro.greeterbro.client.GreeterBroClient;
+import com.padbro.greeterbro.client.JoinCache;
+import com.padbro.greeterbro.client.config.GreeterBroConfig;
+import java.util.List;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class OnPlayerListMixin {
+  @Inject(method = "onPlayerList", at = @At("TAIL"))
+  private void onPlayerList(PlayerListS2CPacket packet, CallbackInfo ci) {
+    GreeterBroConfig config = GreeterBroClient.getConfig();
+    if (GreeterBroClient.isJoining && config.returningPlayerConfig.cacheOnJoin) {
+      List<PlayerListS2CPacket.Entry> players = packet.getPlayerAdditionEntries();
+      JoinCache joinCache = GreeterBroClient.getJoinCache();
+      for (PlayerListS2CPacket.Entry entry : players) {
+        if (entry.profile() != null) {
+          joinCache.add(entry.profile().getName());
+        }
+      }
+      GreeterBroClient.isJoining = false;
+    }
+  }
+}
