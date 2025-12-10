6e62db44f35283f1232c8ce0a284a17c836fae62
PadBro <64410553+PadBro@users.noreply.github.com>
2025-12-10 18:48:56 +0100

    chore: update to 1.21.11

--- src/client/java/com/padbro/greeterbro/client/config/GreeterBroConfig.java
+++ src/client/java/com/padbro/greeterbro/client/config/GreeterBroConfig.java
@@ -6,31 +6,31 @@ import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 
 @Config(name = "GreeterBro")
 public class GreeterBroConfig extends PartitioningSerializer.GlobalData {
-  @ConfigEntry.Category("general")
-  @ConfigEntry.Gui.TransitiveObject
-  public GeneralConfig generalConfig = new GeneralConfig();
+    @ConfigEntry.Category("general")
+    @ConfigEntry.Gui.TransitiveObject
+    public GeneralConfig generalConfig = new GeneralConfig();
 
-  @ConfigEntry.Category("afk")
-  @ConfigEntry.Gui.TransitiveObject
-  public AfkConfig afkConfig = new AfkConfig();
+    @ConfigEntry.Category("afk")
+    @ConfigEntry.Gui.TransitiveObject
+    public AfkConfig afkConfig = new AfkConfig();
 
-  @ConfigEntry.Category("returningPlayer")
-  @ConfigEntry.Gui.TransitiveObject
-  public ReturningPlayerConfig returningPlayerConfig = new ReturningPlayerConfig();
+    @ConfigEntry.Category("returningPlayer")
+    @ConfigEntry.Gui.TransitiveObject
+    public ReturningPlayerConfig returningPlayerConfig = new ReturningPlayerConfig();
 
-  @ConfigEntry.Category("firstJoin")
-  @ConfigEntry.Gui.TransitiveObject
-  public FirstJoinConfig firstJoinConfig = new FirstJoinConfig();
+    @ConfigEntry.Category("firstJoin")
+    @ConfigEntry.Gui.TransitiveObject
+    public FirstJoinConfig firstJoinConfig = new FirstJoinConfig();
 
-  @ConfigEntry.Category("nameChange")
-  @ConfigEntry.Gui.TransitiveObject
-  public NameChangeConfig nameChangeConfig = new NameChangeConfig();
+    @ConfigEntry.Category("nameChange")
+    @ConfigEntry.Gui.TransitiveObject
+    public NameChangeConfig nameChangeConfig = new NameChangeConfig();
 
-  @ConfigEntry.Category("specialGreetings")
-  @ConfigEntry.Gui.TransitiveObject
-  public SpecialGreetingsConfig specialGreetings = new SpecialGreetingsConfig();
+    @ConfigEntry.Category("specialGreetings")
+    @ConfigEntry.Gui.TransitiveObject
+    public SpecialGreetingsConfig specialGreetings = new SpecialGreetingsConfig();
 
-  @ConfigEntry.Category("blacklist")
-  @ConfigEntry.Gui.TransitiveObject
-  public BlacklistConfig blacklistConfig = new BlacklistConfig();
+    @ConfigEntry.Category("blacklist")
+    @ConfigEntry.Gui.TransitiveObject
+    public BlacklistConfig blacklistConfig = new BlacklistConfig();
 }
--- src/client/java/com/padbro/greeterbro/client/config/NameChangeConfig.java
+++ src/client/java/com/padbro/greeterbro/client/config/NameChangeConfig.java
@@ -1,25 +1,28 @@
 package com.padbro.greeterbro.client.config;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Config(name = "nameChange")
 public class NameChangeConfig implements ConfigData {
-  public boolean enable = true;
+    public boolean enable = true;
 
-  @ConfigEntry.Gui.Tooltip public String customMessage = "";
+    @ConfigEntry.Gui.Tooltip
+    public String customMessage = "";
 
-  @ConfigEntry.BoundedDiscrete(max = 100)
-  @ConfigEntry.Gui.Tooltip public int greetingChance = 100;
+    @ConfigEntry.Gui.Tooltip
+    public int greetingChance = 100;
 
-  @ConfigEntry.Gui.Tooltip public List<String> greetings = List.of();
+    @ConfigEntry.Gui.Tooltip
+    public List<String> greetings = List.of();
 
-  @Override
-  public void validatePostLoad() {
-    this.greetings =
-        greetings.stream().filter(s -> !s.trim().isEmpty()).collect(Collectors.toList());
-  }
+    @Override
+    public void validatePostLoad() {
+        this.greetings =
+                greetings.stream().filter(s -> !s.trim().isEmpty()).collect(Collectors.toList());
+    }
 }
