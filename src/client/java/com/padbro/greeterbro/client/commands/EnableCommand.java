db3acfe9c8d51d12d69d5d4c218f98bca5dfff43
PadBro <64410553+PadBro@users.noreply.github.com>
2026-03-27 17:45:52 +0100

    chore: update to 26.1

--- src/client/java/com/padbro/greeterbro/client/commands/EnableCommand.java
+++ src/client/java/com/padbro/greeterbro/client/commands/EnableCommand.java
@@ -8,7 +8,7 @@ import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.literal;
 
 public class EnableCommand {
     public static void register(LiteralArgumentBuilder<FabricClientCommandSource> root) {
